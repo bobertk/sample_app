f356ff273be81b97e3c11e5d72bc3e7746b2ab14
Robert Karlsson <RWKarlsson@yahoo.com>
2012-04-06 10:27:58 -0600

    complete user admin actions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
   	user = User.find_by_email(params[:session][:email])
 	  if user && user.authenticate(params[:session][:password])
 	    sign_in user
-      redirect_to user
+      redirect_back_or user
 	  else
 	    # (flash persists for one request, but — unlike a redirect, re-rendering a 								# template with render doesn’t count as a request)
 	    # so... flash.now contents disappear w next request on rendered pages.
